0313961e3cbdcbe004c120ab484418764b6db213
Simon Hessel <simon.hessel@kreios.lu>
2020-10-13 14:27:48 +0200

    Updates

---
+++ precache-manifest.c77c4b2aefb3c93f12d561b66789c696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03995414ccf77cd3d3a585499986836",
+    "url": "/vertretungsplanapp/index.html"
+  },
+  {
+    "revision": "378f984f954348e38716",
+    "url": "/vertretungsplanapp/static/css/main.aab3a057.chunk.css"
+  },
+  {
+    "revision": "f32ec60352e741872bdb",
+    "url": "/vertretungsplanapp/static/js/2.adab79af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vertretungsplanapp/static/js/2.adab79af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378f984f954348e38716",
+    "url": "/vertretungsplanapp/static/js/main.8a99c029.chunk.js"
+  },
+  {
+    "revision": "214ca469e9347facc657",
+    "url": "/vertretungsplanapp/static/js/runtime-main.302e8cab.js"
+  }
+]);
